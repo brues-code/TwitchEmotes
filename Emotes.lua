0f20f91466dcc88bf8168142c8015cac15f6e554
Brues <5278969+brues-code@users.noreply.github.com>
2026-09-16 19:27:15 -0500

    Add TheVoices
    https://betterttv.com/emotes/63e4728a7b3e63779e666353

--- Emotes.lua
+++ Emotes.lua
@@ -4271,6 +4271,7 @@ defaultpack={
 	["catKISS"]="Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\catKISS.tga:28:28:0:0:64:1024:0:32:0:32",
 	["Maaaaan"]="Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\Maaaaan.tga:28:28",
 	["Tssk"]="Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\Tssk.tga:28:28:0:0:32:416:0:32:0:32",
+	["TheVoices"]="Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\TheVoices.tga:28:28:0:0:128:1024:0:32:0:32",
 
   };
   emoticons={
@@ -8952,6 +8953,7 @@ defaultpack={
 	["catKISS"]="catKISS",
 	["Maaaaan"]="Maaaaan",
 	["Tssk"]="Tssk",
+	["TheVoices"]="TheVoices",
 
   };
 
@@ -9079,4 +9081,5 @@ TwitchEmotes_animation_metadata = {
 	["Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\Awkward.tga"] = {["nFrames"] = 76, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=128, ["imageHeight"]=1024, ["framerate"] = 10},
 	["Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\catKISS.tga"] = {["nFrames"] = 60, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=64, ["imageHeight"]=1024, ["framerate"] = 17},
 	["Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\Tssk.tga"] = {["nFrames"] = 13, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=32, ["imageHeight"]=416, ["framerate"] = 30},
+	["Interface\\AddOns\\TwitchEmotes\\Emotes\\Custom\\TheVoices.tga"] = {["nFrames"] = 127, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]=128, ["imageHeight"]=1024, ["framerate"] = 21},
 }
